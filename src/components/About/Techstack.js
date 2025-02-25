287e088dae6d0e3e9332db088b26fb727ef99e80
Khushi Katariya Work <khushikatariya0911@gmail.com>
2025-02-24 20:36:59 -0800

    Update Techstack

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,72 +1,111 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import { CgCPlusPlus } from "react-icons/cg";
-import {
-  DiJavascript1,
-  DiReact,
-  DiNodejs,
-  DiMongodb,
-  DiPython,
-  DiGit,
-  DiJava,
-} from "react-icons/di";
-import {
-  SiFirebase,  
-  SiPostgresql,
-  SiHtml5,
-  SiCss3,
-  SiMysql,
-  SiBootstrap
-} from "react-icons/si";
-import { TbBrandGolang } from "react-icons/tb";
+import { DiJavascript1, DiPython, DiJava, DiReact, DiNodejs } from "react-icons/di";
+import { SiTypescript, SiMongodb, SiMysql, SiPostgresql, SiNextdotjs, SiTailwindcss, SiAirtable, SiDjango, SiHtml5, SiJson } from "react-icons/si";
+import { SiAmazoncloudwatch, SiAmazonec2, SiAmazonaws, SiAmazondynamodb, SiAmazons3, SiAwslambda } from "react-icons/si";
 
 function Techstack() {
-
-  
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-     
       <Col xs={4} md={2} className="tech-icons">
         <DiJavascript1 />
+        <div className="skill-name">JavaScript</div>
       </Col>
-     
+
       <Col xs={4} md={2} className="tech-icons">
-        <DiNodejs />
+        <SiTypescript />
+        <div className="skill-name">TypeScript</div>
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiReact />
-      </Col>
-      
-      <Col xs={4} md={2} className="tech-icons">
-        <DiMongodb />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiHtml5 />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiCss3 />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiBootstrap />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiGit />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiFirebase />
-      </Col>
-      
-      <Col xs={4} md={2} className="tech-icons">
-        <SiPostgresql />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiMysql />
-      </Col>
+
       <Col xs={4} md={2} className="tech-icons">
         <DiPython />
+        <div className="skill-name">Python</div>
       </Col>
+
       <Col xs={4} md={2} className="tech-icons">
         <DiJava />
+        <div className="skill-name">Java</div>
+      </Col>
+
+      {/* AWS Services */}
+      <Col xs={4} md={2} className="tech-icons">
+        <SiAmazons3 />
+        <div className="skill-name">AWS S3</div>
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <SiAmazondynamodb />
+        <div className="skill-name">AWS DynamoDB</div>
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <SiAmazonec2 />
+        <div className="skill-name">AWS EC2</div>
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <SiAmazoncloudwatch />
+        <div className="skill-name">AWS CloudWatch</div>
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <SiAwslambda />
+        <div className="skill-name">AWS Lambda</div>
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <SiMongodb />
+        <div className="skill-name">MongoDB</div>
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <SiMysql />
+        <div className="skill-name">MySQL</div>
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <SiPostgresql />
+        <div className="skill-name">PostgreSQL</div>
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <SiNextdotjs />
+        <div className="skill-name">NextJS</div>
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <SiDjango />
+        <div className="skill-name">Django</div>
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <SiTailwindcss />
+        <div className="skill-name">Tailwind CSS</div>
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <DiReact />
+        <div className="skill-name">ReactJS</div>
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <DiNodejs />
+        <div className="skill-name">NodeJS</div>
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <SiAirtable />
+        <div className="skill-name">Airtable</div>
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <SiHtml5 />
+        <div className="skill-name">HTML</div>
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <SiJson />
+        <div className="skill-name">JSON</div>
       </Col>
     </Row>
   );
--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import {DiPhotoshop} from "react-icons/di"
+import { DiPhotoshop, DiGit } from "react-icons/di";
 import {
   SiVisualstudiocode,
   SiPostman,
@@ -10,7 +10,11 @@ import {
   SiFigma,
   SiSquarespace,
   SiCanva,
-  SiGithub
+  SiGithub,
+  SiIntellijidea,
+  SiJupyter,
+  SiTableau,
+  SiMicrosoftoffice,
 } from "react-icons/si";
 
 function Toolstack() {
@@ -18,35 +22,64 @@ function Toolstack() {
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
         <SiWindows />
+        <div className="skill-name">Windows</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiVisualstudiocode />
+        <div className="skill-name">VS Code</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiFigma />
+        <div className="skill-name">Figma</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiPostman />
+        <div className="skill-name">Postman</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiSlack />
+        <div className="skill-name">Slack</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiGithub />
+        <div className="skill-name">Github</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <SiCanva/>
+        <SiCanva />
+        <div className="skill-name">Canva</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <DiPhotoshop/>
+        <DiPhotoshop />
+        <div className="skill-name">Photoshop</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiSquarespace />
+        <div className="skill-name">Squarespace</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiVercel />
+        <div className="skill-name">Vercel</div>
       </Col>
-
+      <Col xs={4} md={2} className="tech-icons">
+        <SiIntellijidea />
+        <div className="skill-name">IntelliJ</div>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiJupyter />
+        <div className="skill-name">Jupyter</div>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiTableau />
+        <div className="skill-name">Tableau</div>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiMicrosoftoffice />
+        <div className="skill-name">Microsoft Office</div>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiGit />
+        <div className="skill-name">Git</div>
+      </Col>
     </Row>
   );
 }
