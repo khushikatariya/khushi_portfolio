d49b6ce3c406d8c87de87aca1e87645580cbbe12
Khushi Katariya Work <khushikatariya0911@gmail.com>
2025-02-24 20:02:08 -0800

    Fixed typo for experience link

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ import ScrollToTop from "./components/ScrollToTop";
 import "./style.css";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Experience from "./components/Experience/experience";
+import Experience from "./components/Experience/Experience";
 
 function App() {
   const [load, upadateLoad] = useState(true);
